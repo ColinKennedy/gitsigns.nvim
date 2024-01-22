23d5976df2d5fd458d51f9bc1a0162aacedc1431
Colin Kennedy <colinvfx@gmail.com>
2024-01-21 23:25:57 -0800

    Added extra actions

--- lua/gitsigns/actions.lua
+++ lua/gitsigns/actions.lua
@@ -1512,4 +1512,104 @@ function M._get_cmp_func(name)
   return CP[name]
 end
 
+local function _get_hunk_range(hunk)
+  local start = nil
+  local count = nil
+
+  if hunk.added ~= nil
+  then
+    start = hunk.added.start
+    count = hunk.added.count
+  end
+
+  if hunk.removed ~= nil
+  then
+    if hunk.removed.start < start
+    then
+      start = hunk.removed.start
+    end
+
+    if hunk.removed.count > count
+    then
+      count = hunk.removed.count
+    end
+  end
+
+  return {start, start + count}
+end
+
+M.move_to_first_hunk = async.void(function(opts, forwards)
+  async.scheduler_if_buf_valid(current_buf())
+
+  local hunks = M.get_head_hunks() or {}
+
+  if vim.tbl_isempty(hunks)
+  then
+      return
+  end
+
+  local hunk = hunks[1]
+  local line = hunk.added.start or hunk.removed.start
+
+  vim.api.nvim_win_set_cursor(0, {line, 0})
+end)
+
+function M.get_head_hunks()
+  local bufnr = current_buf()
+  local bcache = cache[bufnr]
+
+  if not bcache then
+    return nil
+  end
+
+  local greedy = false
+
+  local hunks = {}
+  vim.list_extend(hunks, get_hunks(bufnr, bcache, greedy, false) or {})
+  local hunks_head = get_hunks(bufnr, bcache, greedy, true) or {}
+  vim.list_extend(hunks, Hunks.filter_common(hunks_head, bcache.hunks) or {})
+
+  return hunks or nil
+end
+
+function M.in_hunk()
+  local hunks = M.get_head_hunks() or {}
+
+  if vim.tbl_isempty(hunks) then
+    return false
+  end
+
+  local line = api.nvim_win_get_cursor(0)[1]
+
+  for index = 1, #hunks do
+    local hunk = hunks[index]
+    local start_line, end_line = unpack(_get_hunk_range(hunk))
+
+    if line >= start_line and line <= end_line
+    then
+      return true
+    end
+  end
+
+  return false
+end
+
+function M.has_next_hunk(forwards)
+  local hunks = M.get_head_hunks() or {}
+
+  if vim.tbl_isempty(hunks) then
+    return false
+  end
+
+  local line = api.nvim_win_get_cursor(0)[1]
+  local wrap = false
+  local hunk, _ = Hunks.find_nearest_hunk(line, hunks, forwards, wrap)
+
+  if hunk == nil then
+    return false
+  end
+
+  return true
+end
+
 return M
